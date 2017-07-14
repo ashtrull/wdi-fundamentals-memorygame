dc563be74e1edf62019548b61057ec517f01de4c
ashtrull@gmail.com <ashtrull@gmail.com>
2017-07-14 10:18:53 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,16 @@
 console.log("Up and running!");
-var cardOne = "king";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "queen";
 
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else if (cardsInPlay.length === 2) {
+  console.log("Sorry, try again.");
+} else {console.log("Please flip another card");
+}
